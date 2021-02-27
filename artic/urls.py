26277518f8f0820e3b0de051a6e2e895c459366f
mostafavfatemeh <mostafahkr20@gmail.com>
2021-02-27 16:54:20 +0330

    film 11,12

--- artic/urls.py
+++ artic/urls.py
@@ -2,8 +2,8 @@
 from django.urls import path
 from . import views
 
-
+app_name="dastan"
 urlpatterns = [
-    path('',views.articles_list),
-    
+    path('',views.articles_list,name="list"),
+    path('<slugg>',views.artic_detail,name="detail"),
 ]
--- artic/views.py
+++ artic/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from . import models
 
 def articles_list(request):
     arti=models.Dastan.objects.all().order_by('date')
     args={'artii':arti}
     return render(request,'articc/artic_list.html',args)
+
+def artic_detail(request,slugg):
+    return HttpResponse(slugg)
