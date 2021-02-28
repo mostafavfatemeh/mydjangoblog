d45b68dec16630903d8b8eb7ebc1cec67d8b47ba
mostafavfatemeh <mostafahkr20@gmail.com>
2021-02-28 23:04:23 +0330

    film14

--- djangoBlogg/urls.py
+++ djangoBlogg/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,4 @@ urlpatterns = [
     path('articles/',include('artic.urls')),
 ]
 urlpatterns +=staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
